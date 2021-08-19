ce7f05fc2e913b080ab5259bb00007143093f17d
N3M355i5 <aryanvarshney1@gmail.com>
2021-08-19 17:39:37 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:moi_movies/pages/home_page.dart';
+import 'package:moi_movies/pages/login_page.dart';
 
     void main() {
       runApp(MyApp());
@@ -11,7 +13,17 @@ import 'package:moi_movies/pages/home_page.dart';
       @override
       Widget build(BuildContext context) {
         return MaterialApp(
-          home: HomePage(),
+          themeMode: ThemeMode.dark,
+          theme: ThemeData(
+            primarySwatch: Colors.deepPurple
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+          ),
+          routes: {
+            "/" : (context) => HomePage(),
+            "/login" : (context) => LoginPage(),
+          },
         );
       }
     }
